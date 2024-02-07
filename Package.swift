508b64324851e92e396b022690369affaea8ad2f
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2024-02-07 10:54:07 +0100

    Add tvOS to supported platforms

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ETTrace",
-    platforms: [.iOS(.v13), .macOS(.v12)],
+    platforms: [.iOS(.v13), .macOS(.v12), .tvOS(.v13)],
     products: [
         .library(
             name: "ETTrace",
